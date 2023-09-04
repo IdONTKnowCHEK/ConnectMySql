8e57ce428d26abda693a8b225ba390fa9ad4457e
“chak12346789@gmail.com” <chak12346789@gmail.com>
2023-09-04 16:47:02 +0800

    加入專案檔案。

---
+++ ConnectMySql/Controllers/MySqlController.cs
@@ -0,0 +1,58 @@
+﻿using ConnectMySql.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ConnectMySql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MySqlController : ControllerBase
+    {
+
+        private readonly LineBotApiPracticeContext _context;
+
+        public MySqlController(LineBotApiPracticeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<MySqlController>
+        [HttpGet]
+        public IEnumerable<Linebot> Get()
+        {
+            return _context.Linebots.ToList();
+        }
+
+        // GET api/<MySqlController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MySqlController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MySqlController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MySqlController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpPost("Webhook")]
+        public IActionResult Webhook()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ConnectMySql/Models/LineBotApiPracticeContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectMySql.Models;
+
+public partial class LineBotApiPracticeContext : DbContext
+{
+    public LineBotApiPracticeContext()
+    {
+    }
+
+    public LineBotApiPracticeContext(DbContextOptions<LineBotApiPracticeContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Linebot> Linebots { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySQL("Name=ConnectionString:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Linebot>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("linebot");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Major)
+                .HasMaxLength(20)
+                .HasColumnName("major");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
